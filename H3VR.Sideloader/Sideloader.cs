d1de4a065e4fb0ed63a0d0e17474d40242a61e9e
ghorsington <neigh@coder.horse>
2020-07-23 11:33:48 +0300

    Initial commit

---
+++ H3VR.Sideloader/Sideloader.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+
+namespace H3VR.Sideloader
+{
+    [BepInPlugin("horse.coder.h3vr.sideloader", NAME, VERSION)]
+    [BepInDependency("gravydevsupreme.xunity.resourceredirector")]
+    public class Sideloader : BaseUnityPlugin
+    {
+        internal const string VERSION = "1.0.0";
+        internal const string NAME = "H3VR Sideloader";
+    }
+}
