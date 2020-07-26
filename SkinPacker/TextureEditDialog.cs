7b8b5223553108bef41ce570f1b2f263ab623f0e
ghorsington <neigh@coder.horse>
2020-07-26 12:10:11 +0300

    Add initial texture edit dialog

---
+++ SkinPacker/TextureEditDialog.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace SkinPacker
+{
+    public partial class TextureEditDialog : Form
+    {
+        public TextureEditDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
