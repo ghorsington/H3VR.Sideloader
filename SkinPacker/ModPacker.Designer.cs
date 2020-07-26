a53be8dc54464bfc343521e9fc0b771174029fe6
ghorsington <neigh@coder.horse>
2020-07-26 12:07:37 +0300

    Change packer dialog layout

--- SkinPacker/ModPacker.Designer.cs
+++ SkinPacker/ModPacker.Designer.cs
@@ -48,23 +48,24 @@ namespace SkinPacker
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.progressLabel, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.progressBar, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 3);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 50, 0, 30);
-            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 30, 0, 10);
+            this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(494, 221);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(494, 171);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // progressLabel
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.progressLabel, 2);
             this.progressLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.progressLabel.Location = new System.Drawing.Point(3, 50);
+            this.progressLabel.Location = new System.Drawing.Point(3, 30);
             this.progressLabel.Name = "progressLabel";
             this.progressLabel.Size = new System.Drawing.Size(488, 23);
             this.progressLabel.TabIndex = 0;
@@ -74,23 +75,22 @@ namespace SkinPacker
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.progressBar, 2);
             this.progressBar.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.progressBar.Location = new System.Drawing.Point(3, 76);
+            this.progressBar.Location = new System.Drawing.Point(3, 56);
             this.progressBar.Name = "progressBar";
             this.progressBar.Size = new System.Drawing.Size(488, 23);
             this.progressBar.TabIndex = 1;
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.AutoSize = true;
             this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 2);
             this.flowLayoutPanel1.Controls.Add(this.closButton);
             this.flowLayoutPanel1.Controls.Add(this.openFolderButton);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 160);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 125);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 30);
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(488, 59);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(488, 33);
             this.flowLayoutPanel1.TabIndex = 2;
             this.flowLayoutPanel1.WrapContents = false;
             // 
@@ -121,7 +121,7 @@ namespace SkinPacker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 221);
+            this.ClientSize = new System.Drawing.Size(494, 171);
             this.ControlBox = false;
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -133,7 +133,6 @@ namespace SkinPacker
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Packaging mod";
             this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
