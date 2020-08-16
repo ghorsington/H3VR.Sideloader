aee7bdc5a866d82a3bf4b73c9bb708f87916b12f
ghorsington <neigh@coder.horse>
2020-08-16 12:49:24 +0300

    Add initial audio source hook

--- H3VR.Sideloader/AssetLoaders/AudioClipLoader.cs
+++ H3VR.Sideloader/AssetLoaders/AudioClipLoader.cs
@@ -1,6 +1,21 @@
-﻿namespace H3VR.Sideloader.AssetLoaders
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using UnityEngine;
+
+namespace H3VR.Sideloader.AssetLoaders
 {
-    internal class AudioClipLoader
+    internal class AudioClipLoader : ILoader
     {
+        public void Initialize(IEnumerable<Mod> mods)
+        {
+            Harmony.CreateAndPatchAll(typeof(AudioClipLoader));
+        }
+
+        [HarmonyPatch(typeof(AudioSource), nameof(AudioSource.Play))]
+        [HarmonyPrefix]
+        private static void OnAudioSourcePlay(AudioSource __instance)
+        {
+            Sideloader.Logger.LogInfo($"Playing ${__instance.name} with audio clip: {__instance.clip}");
+        }
     }
 }
