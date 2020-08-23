f4d42bb7effaf40de319e80633724986b936e41e
ghorsington <neigh@coder.horse>
2020-08-23 17:08:24 +0300

    Fix ZIP encoding in patcher

--- H3VR.Sideloader.MonoMod/Patcher.cs
+++ H3VR.Sideloader.MonoMod/Patcher.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using BepInEx;
 using BepInEx.Logging;
 using H3VR.Sideloader.Shared;
 using ICSharpCode.SharpZipLib.Zip;
 using Mono.Cecil;
-using MonoMod;
 
 namespace H3VR.Sideloader.MonoMod
 {
@@ -52,6 +52,7 @@ namespace H3VR.Sideloader.MonoMod
 
         public static void Initialize()
         {
+            ZipConstants.DefaultCodePage = Encoding.UTF8.CodePage;
             try
             {
                 assemblyStreams = Init();
@@ -118,65 +119,6 @@ namespace H3VR.Sideloader.MonoMod
 
             using var modder = new SideloaderMonoModder(ass, Logger);
             modder.Run(assemblyStreams);
-        }
-    }
-
-    internal class SideloaderMonoModder : MonoModder
-    {
-        private readonly ManualLogSource logger;
-
-        public SideloaderMonoModder(AssemblyDefinition assembly, ManualLogSource logger)
-        {
-            Module = assembly.MainModule;
-            this.logger = logger;
-        }
-
-        public override void Log(string text)
-        {
-            logger.LogMessage(text);
-        }
-
-        public override void Log(object value)
-        {
-            logger.LogMessage(value);
-        }
-
-        public override void LogVerbose(object value)
-        {
-            logger.LogDebug(value);
-        }
-
-        public override void LogVerbose(string text)
-        {
-            logger.LogDebug(text);
-        }
-
-        public void Run(IEnumerable<Stream> assemblies)
-        {
-            Read();
-
-            Log("Scanning mods");
-
-            foreach (var assembly in assemblies)
-                ReadMod(assembly);
-
-            MapDependencies();
-
-            Log($"Found {Mods.Count} mods");
-
-            Log("mm.PatchRefs");
-            PatchRefs();
-
-            Log("mm.AutoPatch");
-            AutoPatch();
-
-            Log("Done");
-        }
-
-        public override void Dispose()
-        {
-            Module = null;
-            base.Dispose();
         }
     }
 }
---
+++ H3VR.Sideloader.MonoMod/SideloaderMonoModder.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using BepInEx.Logging;
+using Mono.Cecil;
+using MonoMod;
+
+namespace H3VR.Sideloader.MonoMod
+{
+    internal class SideloaderMonoModder : MonoModder
+    {
+        private readonly ManualLogSource logger;
+
+        public SideloaderMonoModder(AssemblyDefinition assembly, ManualLogSource logger)
+        {
+            Module = assembly.MainModule;
+            this.logger = logger;
+        }
+
+        public override void Log(string text)
+        {
+            logger.LogMessage(text);
+        }
+
+        public override void Log(object value)
+        {
+            logger.LogMessage(value);
+        }
+
+        public override void LogVerbose(object value)
+        {
+            logger.LogDebug(value);
+        }
+
+        public override void LogVerbose(string text)
+        {
+            logger.LogDebug(text);
+        }
+
+        public void Run(IEnumerable<Stream> assemblies)
+        {
+            Read();
+
+            Log("Scanning mods");
+
+            foreach (var assembly in assemblies)
+                ReadMod(assembly);
+
+            MapDependencies();
+
+            Log($"Found {Mods.Count} mods");
+
+            if (Mods.Count == 0)
+                return;
+
+            Log("mm.PatchRefs");
+            PatchRefs();
+
+            Log("mm.AutoPatch");
+            AutoPatch();
+
+            Log("Done");
+        }
+
+        public override void Dispose()
+        {
+            Module = null;
+            base.Dispose();
+        }
+    }
+}
