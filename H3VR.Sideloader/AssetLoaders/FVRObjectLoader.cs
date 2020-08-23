816cdae472fdf5d2f08e9f0ac6c7cd22504d2afe
ghorsington <neigh@coder.horse>
2020-08-23 22:39:42 +0300

    Clean up code

--- H3VR.Sideloader/AssetLoaders/FVRObjectLoader.cs
+++ H3VR.Sideloader/AssetLoaders/FVRObjectLoader.cs
@@ -66,6 +66,7 @@ namespace H3VR.Sideloader.AssetLoaders
         [HarmonyPostfix]
         private static void InjectObjects(IM __instance)
         {
+            var im = __instance;
             foreach (var itemSpawnerId in ItemSpawnerIds)
             {
                 IM.CD[itemSpawnerId.Category].Add(itemSpawnerId);
@@ -76,16 +77,16 @@ namespace H3VR.Sideloader.AssetLoaders
             {
                 IM.OD.Add(fvrObject.ItemID, fvrObject);
 
-                __instance.odicTagCategory.AddOrCreate(fvrObject.Category).Add(fvrObject);
-                __instance.odicTagFirearmEra.AddOrCreate(fvrObject.TagEra).Add(fvrObject);
-                __instance.odicTagFirearmSize.AddOrCreate(fvrObject.TagFirearmSize).Add(fvrObject);
-                __instance.odicTagFirearmAction.AddOrCreate(fvrObject.TagFirearmAction).Add(fvrObject);
-                __instance.odicTagFirearmFiringMode.AddOrCreate(fvrObject.TagFirearmFiringModes.FirstOrDefault()).Add(fvrObject);
-                __instance.odicTagFirearmFeedOption.AddOrCreate(fvrObject.TagFirearmFeedOption.FirstOrDefault()).Add(fvrObject);
-                __instance.odicTagFirearmMount.AddOrCreate(fvrObject.TagFirearmMounts.FirstOrDefault()).Add(fvrObject);
-                __instance.odicTagAttachmentMount.AddOrCreate(fvrObject.TagAttachmentMount).Add(fvrObject);
-                __instance.odicTagAttachmentFeature.AddOrCreate(fvrObject.TagAttachmentFeature).Add(fvrObject);
-            }    
+                im.odicTagCategory.AddOrCreate(fvrObject.Category).Add(fvrObject);
+                im.odicTagFirearmEra.AddOrCreate(fvrObject.TagEra).Add(fvrObject);
+                im.odicTagFirearmSize.AddOrCreate(fvrObject.TagFirearmSize).Add(fvrObject);
+                im.odicTagFirearmAction.AddOrCreate(fvrObject.TagFirearmAction).Add(fvrObject);
+                im.odicTagFirearmFiringMode.AddOrCreate(fvrObject.TagFirearmFiringModes.FirstOrDefault()).Add(fvrObject);
+                im.odicTagFirearmFeedOption.AddOrCreate(fvrObject.TagFirearmFeedOption.FirstOrDefault()).Add(fvrObject);
+                im.odicTagFirearmMount.AddOrCreate(fvrObject.TagFirearmMounts.FirstOrDefault()).Add(fvrObject);
+                im.odicTagAttachmentMount.AddOrCreate(fvrObject.TagAttachmentMount).Add(fvrObject);
+                im.odicTagAttachmentFeature.AddOrCreate(fvrObject.TagAttachmentFeature).Add(fvrObject);
+            }
         }
 
         [HarmonyPatch(typeof(AnvilManager), "GetAssetBundleAsyncInternal")]
@@ -99,7 +100,8 @@ namespace H3VR.Sideloader.AssetLoaders
                 .SetAndAdvance(OpCodes.Nop, null)
                 .Insert(
                     new CodeInstruction(OpCodes.Ldarg_0),
-                    Transpilers.EmitDelegate((Func<string, string, bool>) ((path, bundle) => AbAssets.ContainsKey(bundle) || File.Exists(path))))
+                    Transpilers.EmitDelegate((Func<string, string, bool>) ((path, bundle) =>
+                        AbAssets.ContainsKey(bundle) || File.Exists(path))))
                 .MatchForward(false,
                     new CodeMatch(OpCodes.Call,
                         AccessTools.Method(typeof(AssetBundle), nameof(AssetBundle.LoadFromFileAsync),
@@ -108,11 +110,11 @@ namespace H3VR.Sideloader.AssetLoaders
                 .Insert(
                     new CodeInstruction(OpCodes.Ldarg_0),
                     Transpilers.EmitDelegate((Func<string, string, AsyncOperation>) ((path, bundle) =>
-                {
-                    if (AbAssets.TryGetValue(bundle, out var abAsset))
-                        return new AnvilDummyOperation(abAsset.Mod.LoadAssetBundle(abAsset.Path, out _));
-                    return AssetBundle.LoadFromFileAsync(path);
-                })))
+                    {
+                        if (AbAssets.TryGetValue(bundle, out var abAsset))
+                            return new AnvilDummyOperation(abAsset.Mod.LoadAssetBundle(abAsset.Path, out _));
+                        return AssetBundle.LoadFromFileAsync(path);
+                    })))
                 .InstructionEnumeration();
         }
 
@@ -125,7 +127,8 @@ namespace H3VR.Sideloader.AssetLoaders
 
     public static class DictionaryExtension
     {
-        public static TValue AddOrCreate<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key) where TValue : new()
+        public static TValue AddOrCreate<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key)
+            where TValue : new()
         {
             if (!dictionary.ContainsKey(key))
                 dictionary.Add(key, new TValue());
