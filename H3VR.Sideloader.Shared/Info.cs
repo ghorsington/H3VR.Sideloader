845d7a14acb4d5793e771875ea93ccdc3c8fdfde
ghorsington <neigh@coder.horse>
2021-07-12 09:23:53 +0300

    Bump version

--- H3VR.Sideloader.Shared/Info.cs
+++ H3VR.Sideloader.Shared/Info.cs
@@ -2,9 +2,9 @@
 {
     internal static class Info
     {
-        public const string VERSION = "0.3.5";
+        public const string VERSION = "0.3.6";
         public const string NAME = "H3VR Sideloader";
-        public const string LICENSE = "MIT: Geoffrey Horsington 2020";
+        public const string LICENSE = "MIT: Geoffrey Horsington 2021";
         public static readonly string[] ModExts = {"h3mod", "hotmod"};
     }
 }
